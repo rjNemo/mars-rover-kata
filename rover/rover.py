f45f7e0fda4edb4552bcc14dbfaec3262e9b4cad
Ruidy <r.nemausat@empfohlen.de>
2021-08-27 15:12:18 +0200

    use Coordinates class and type hints

--- rover/rover.py
+++ rover/rover.py
@@ -1,30 +1,39 @@
-import dataclasses
+from dataclasses import dataclass, field
+from typing import Tuple
 
 
-@dataclasses.dataclass
+@dataclass(frozen=True)
+class Coordinates:
+    x: int
+    y: int
+    left: str
+    right: str
+
+
+@dataclass
 class Rover:
     x: int
     y: int
     direction: str
-    obstacles: list[list[int]] = dataclasses.field(default_factory=list)
+    obstacles: list[list[int]] = field(default_factory=list)
 
     valid_commands = ("F", "B", "R", "L")
 
     direction_map = {
-        "NORTH": (0, 1, "WEST", "EAST"),
-        "EAST": (1, 0, "NORTH", "SOUTH"),
-        "SOUTH": (0, -1, "EAST", "WEST"),
-        "WEST": (-1, 0, "SOUTH", "NORTH"),
+        "NORTH": Coordinates(0, 1, "WEST", "EAST"),
+        "EAST": Coordinates(1, 0, "NORTH", "SOUTH"),
+        "SOUTH": Coordinates(0, -1, "EAST", "WEST"),
+        "WEST": Coordinates(-1, 0, "SOUTH", "NORTH"),
     }
 
-    def is_valid_command(self, command):
+    def is_valid_command(self, command: str) -> bool:
         for ch in command:
             if ch not in self.valid_commands:
                 return False
 
         return True
 
-    def is_obstacle(self, x: int, y: int):
+    def is_obstacle(self, x: int, y: int) -> bool:
         return [x, y] in self.obstacles
 
     def move(self, command: str):
@@ -34,36 +43,36 @@ class Rover:
         for ch in command:
             x, y, direction = self._compute_new_coordinates(ch)
             if obstacle := self.is_obstacle(x, y):
-                return self.new_coordinates_output(obstacle)
+                return self._new_coordinates_output(obstacle)
 
-            self.apply_new_coordinates(x, y, direction)
+            self._apply_new_coordinates(x, y, direction)
 
-        return self.new_coordinates_output()
+        return self._new_coordinates_output()
 
-    def new_coordinates_output(self, obstacle: bool = False):
+    def _new_coordinates_output(self, obstacle: bool = False):
         if obstacle:
             return self.x, self.y, self.direction, "STOPPED"
         return self.x, self.y, self.direction
 
-    def _compute_new_coordinates(self, command):
+    def _compute_new_coordinates(self, command: str) -> Tuple[int, int, str]:
         x = self.x
         y = self.y
         direction = self.direction
 
         if command == "B":
-            x -= self.direction_map[self.direction][0]
-            y -= self.direction_map[self.direction][1]
+            x -= self.direction_map[self.direction].x
+            y -= self.direction_map[self.direction].y
         if command == "F":
-            x += self.direction_map[self.direction][0]
-            y += self.direction_map[self.direction][1]
+            x += self.direction_map[self.direction].x
+            y += self.direction_map[self.direction].y
         if command == "R":
-            direction = self.direction_map[self.direction][3]
+            direction = self.direction_map[self.direction].right
         if command == "L":
-            direction = self.direction_map[self.direction][2]
+            direction = self.direction_map[self.direction].left
 
         return x, y, direction
 
-    def apply_new_coordinates(self, x, y, direction):
+    def _apply_new_coordinates(self, x: int, y: int, direction: str) -> None:
         self.x = x
         self.y = y
         self.direction = direction
